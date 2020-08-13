cc9fc65e3d6b33d203003d56896849e9acd94e9a
Vasya Bogatchuk <v380962437840@gmail.com>
2020-08-13 19:47:13 +0300

    no message

---
+++ Normal-3_24/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normal_3_24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string sentence = Console.ReadLine();
+            string[] str = sentence.Split(' ');
+            int maxWord = 0;
+            int indexMax = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (str[i].Length > maxWord)
+                {
+                    maxWord = str[i].Length;
+                    indexMax = i;
+                }  
+
+            }
+
+            Console.Write("Самое длинное слово: {0}\n", str[indexMax]);
+            Console.ReadLine();
+        }
+    }
+}
